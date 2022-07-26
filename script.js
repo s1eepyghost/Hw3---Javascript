4ee0d0081e85fa9359ec7375372fb368cdfe0592
SleepyGhost <libenwarsame5@gmail.com>
2022-07-25 22:29:43 -0500

    Fixing javascript

--- script.js
+++ script.js
@@ -1,51 +1,54 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var passwordText = document.querySelector("#password");
+var specialCharacters = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numericalCharacters = "0123456789";
+var selectedCharacters = [];
 
 // Write password to the #password input
-function writePassword() {
+function writePassword() { 
   var password = generatePassword();
-  var specialCharacters = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-  var lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
-  var uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var numericalCharacters = "0123456789";
-  var selectedCharacters = [];
-
   passwordText.value = password;
-
 }
 
-function writePassword() {
-  event.preventDefault();
+function generatePassword() {
   password.value === "";
   var randomString = "";
   var passwordLength = prompt("How many characters do you want in your password? --Min:8 Max:128--");
+  
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+    alert("Your password isn't inbetween 8 and 128 characters, please try again");
+    writePassword();
+  }
+
   var uppercase = confirm("Do you want your password to have an uppercase letter?");
   var lowercase = confirm("Do you want your password to have a lowercase letter?");
   var numerical = confirm("Do you want your password to have a number?");
   var special = confirm("Do you want your password to have any special characters?");
-  if (passwordLength < 8 || passwordLength > 128) {
-    alert("Your password isn't inbetween 8 and 128 characters, please try again");
-  }
-  {
 
-  }
   if (uppercase) {
-    selectedCharacters + uppercaseCharacters;
+    selectedCharacters += uppercaseCharacters;
   }
   if (lowercase) {
-    selectedCharacters + lowercaseCharacters;
+    selectedCharacters += lowercaseCharacters;
   }
   if (numerical) {
-    selectedCharacters + numericalCharacters;
+    selectedCharacters += numericalCharacters;
   }
   if (special) {
-    selectedCharacters + specialCharacters;
+    selectedCharacters += specialCharacters;
   } 
-  for (var i = 0; i < passwordLength; i++) {
-    randomString + selectedCharacters.charAt(Math.floor(Math.random() * selectedCharacters.length));
+  if (uppercase + lowercase + numerical + special === 0) {
+    alert("You need to select at least one");
+    writePassword()
   }
-  password.value = randomString;
+  for (var i = 0; i < passwordLength; i++) {
+    randomString += selectedCharacters.charAt(Math.floor(Math.random() * selectedCharacters.length));
+  }
+  console.log(randomString);
+  return randomString
 }
 
 // Add event listener to generate button
